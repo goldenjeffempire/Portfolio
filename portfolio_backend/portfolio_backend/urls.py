f414bfdd1e8e820857cb1f58502bbb4f1ead2379
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-07-09 18:18:21 +0100

    tzdata installed and working

--- portfolio_backend/portfolio_backend/urls.py
+++ portfolio_backend/portfolio_backend/urls.py
@@ -4,12 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from portfolio.views import custom_404, custom_500
+from django.views.static import serve
+
 
 # Custom error handlers
 handler404 = custom_404
 handler500 = custom_500
 
 urlpatterns = [
+    path("favicon.ico", serve, {"path": "favicon.ico", "document_root": settings.STATIC_ROOT}),
     path("admin/", admin.site.urls),
 
     # ✅ Register namespaced API only ONCE
